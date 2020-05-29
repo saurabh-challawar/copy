f3abc5435ebdb024422e7a84f08a12fb8711e05d
Saurabh Challawar <saurabhchallawar@yahoo.com>
2020-05-29 18:39:21 +0530

    for srinivas to check

---
+++ app/src/main/java/com/example/youtubecopy/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.youtubecopy;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.youtubecopy.R;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void play(View v) {
+        if (player == null) {
+            player = MediaPlayer.create(this, R.raw.lel);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+    public void pause(View v) {
+        if (player != null) {
+            player.pause();
+        }
+    }
+    public void stop(View v) {
+        stopPlayer();
+    }
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this, "MediaPlayer released", Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
+}
+
